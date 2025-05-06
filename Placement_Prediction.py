516d8a991f7bca475c22176aafa271dbb2eca8db
Prem Mohan <premmohan966@gmail.com>
2025-05-06 15:28:15 +0530

    Update Placement_Prediction.py

--- Placement_Prediction.py
+++ Placement_Prediction.py
@@ -8,9 +8,9 @@ def main():
     cgpa = st.slider("Choose your CGPA", min_value=0.0, max_value=10.0, step=0.1)
     st.write("Entered CGPA:", cgpa)
 
-    if st.button("Predict"):
-        result = model.predict([[cgpa]])[0]  # Wrap cgpa in a list of list for sklearn
-        st.success(f"Your predicted package would be: {result:.2f} LPA")
+if st.button("Predict"):
+    result = model.predict([cgpa])[0]
+    st.success(f"Your predicted package would be: {result:} LPA")
 
 if __name__ == '__main__':
     main()
